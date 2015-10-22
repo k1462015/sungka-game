4e75b726fef4cbb89df01faf50b29a907e47509e
AnanaMJ <ana.jalba13@gmail.com>
2015-10-22 15:03:21 +0100

    Created all required buttons for trays and stores

--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainActivity.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     Board gameBoard;
+    Button[] boardButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +29,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setupBoardLayout(){
+
+        boardButtons = new Button[16];
+
         LinearLayout layout_p2_store = (LinearLayout) findViewById(R.id.layout_p2_store);
         LinearLayout layout_p1_store = (LinearLayout) findViewById(R.id.layout_p1_store);
         LinearLayout layout_p2_trays = (LinearLayout) findViewById(R.id.layout_p2_trays);
         LinearLayout layout_p1_trays = (LinearLayout) findViewById(R.id.layout_p1_trays);
 
 
-        Button storeButtonp1 = (Button) getLayoutInflater().inflate(R.layout.stores, layout_p1_store, true);
-        Button storeButtonp2 = (Button) getLayoutInflater().inflate(R.layout.stores, layout_p2_store, true);
+        Button storeButtonp1 = (Button) getLayoutInflater().inflate(R.layout.stores, layout_p1_store, false);
+        Button storeButtonp2 = (Button) getLayoutInflater().inflate(R.layout.stores, layout_p2_store, false);
+
+        //store buttons n the array
+        boardButtons[7] = storeButtonp1;
+        boardButtons[15] = storeButtonp2;
+
+        //setting the text for the buttons
+        storeButtonp1.setText("Player 1 store");
+        storeButtonp2.setText("Player 2 store");
+
+        //adding the buttons to the layout
+        layout_p1_store.addView(storeButtonp1);
+        layout_p2_store.addView(storeButtonp2);
+
+        for(int i = 0; i < 7; i++){
+            Button tempTrayp1 = (Button) getLayoutInflater().inflate(R.layout.tray, layout_p1_trays, false);
+            tempTrayp1.setText("Tray "+i);
+            boardButtons[i] = tempTrayp1;
+            layout_p1_trays.addView(tempTrayp1);
+
+            Button tempTrayp2 = (Button) getLayoutInflater().inflate(R.layout.tray, layout_p2_trays, false);
+            tempTrayp2.setText("Tray "+(14-i));
+            boardButtons[14-i] = tempTrayp2;
+            layout_p2_trays.addView(tempTrayp2);
+        }
     }
 }
