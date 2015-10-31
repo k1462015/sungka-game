1c58edc68bc96b71b6260058a4e0dd0b09fa8530
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-10-31 23:24:19 +0000

    Made the main board activity full screen. Also added some images needed for the main menu.

--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainActivity.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainActivity.java
@@ -1,6 +1,7 @@
 package uk.ac.kcl.teamraccoon.sungka;
 
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -44,6 +45,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
     }
 
     /**
