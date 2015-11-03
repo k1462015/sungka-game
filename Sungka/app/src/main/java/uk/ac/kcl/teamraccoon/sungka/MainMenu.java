063fab6c7abc69dd2fdcb44cd38128f4db42e0ca
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-11-03 16:24:45 +0000

    Added smaller sized images as previous images were almost half a MB each!

--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainMenu.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainMenu.java
@@ -3,10 +3,7 @@ package uk.ac.kcl.teamraccoon.sungka;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
