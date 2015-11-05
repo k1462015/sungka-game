8e2a0c205270afb70b0225149f2d230574f6febd
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-11-05 22:08:26 +0000

    Revamped the main menu. to show a racoon.  Provided the option to start in P1 vs P2 mode or to start with P1 vs Ai. Also made the high score table button functional. Exit button also exits application.

--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainMenu.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainMenu.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import uk.ac.kcl.teamraccoon.sungka.highscores.AddScoreFragment;
+import uk.ac.kcl.teamraccoon.sungka.highscores.HighScoresActivity;
+
 public class MainMenu extends AppCompatActivity {
+    public final static String GAME_OPTION = "com.uk.ac.kcl.teamraccoon.sungka.OPTION_P1_P2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +18,31 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
     }
 
-    public void startGame(View view){
+    public void startP1P2(View view){
         Intent intent = new Intent(this,MainActivity.class);
-        finish();
+        intent.putExtra(GAME_OPTION, "P1P2");
         startActivity(intent);
+    }
+
+    public void startP1Comp(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.putExtra(GAME_OPTION,"P1Comp");
+        startActivity(intent);
+    }
+
+    public void startMultiplayer(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.putExtra(GAME_OPTION,"Multiplayer");
+        startActivity(intent);
+    }
+
+    public void showHighScores(View view){
+        Intent intent = new Intent(this, HighScoresActivity.class);
+        startActivity(intent);
+    }
+
+    public void exitGame(View view){
+        System.exit(0);
     }
 
     @Override
