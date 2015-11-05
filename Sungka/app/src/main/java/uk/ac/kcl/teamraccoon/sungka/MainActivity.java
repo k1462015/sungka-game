760e36707e09ec84e5881ad198e65e34439c62d0
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-11-05 19:39:53 +0000

    Added some code to make app full screen if phone SDK version is high enough.

--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainActivity.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/MainActivity.java
@@ -1,6 +1,7 @@
 package uk.ac.kcl.teamraccoon.sungka;
 
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -373,7 +374,7 @@ public class MainActivity extends AppCompatActivity {
                 handler.removeCallbacks(this);
             }
         };
-        handler.postDelayed(aiMove,2500);
+        handler.postDelayed(aiMove, 2500);
     }
 
     public void setupBoardLayout() {
@@ -471,6 +472,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         updateBoard();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
     }
 
     // Display a dialog to save game result and show statistics
