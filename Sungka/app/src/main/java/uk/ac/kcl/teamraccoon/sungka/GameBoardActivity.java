3379a1aac944a53cf50d0bebbc61cecc337cccd7
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-11-12 14:38:27 +0000

    Revert "feat: Set the default name for AI"
    This reverts commit 4edfc9bb4f8ed06e0b6328bd09758bab591a413c.

--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/GameBoardActivity.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/GameBoardActivity.java
@@ -13,11 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
+import android.view.ViewGroup;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
@@ -29,6 +29,7 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.Random;
 
+import uk.ac.kcl.teamraccoon.sungka.data.PlayerData;
 import uk.ac.kcl.teamraccoon.sungka.highscores.AddScoreFragment;
 import uk.ac.kcl.teamraccoon.sungka.online.OnlineClient;
 import uk.ac.kcl.teamraccoon.sungka.online.OnlineServer;
@@ -878,7 +879,7 @@ public class GameBoardActivity extends AppCompatActivity {
         int[] scores = {arrayOfTrays[7], arrayOfTrays[15]};
         bundleDialog.putIntArray(AddScoreFragment.BUNDLE_TAG, scores);
 
-        AddScoreFragment addScoreFragment = new AddScoreFragment(aiChosen);
+        AddScoreFragment addScoreFragment = new AddScoreFragment();
         addScoreFragment.setArguments(bundleDialog);
         addScoreFragment.show(getFragmentManager(), "dialog");
     }
--- Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/highscores/AddScoreFragment.java
+++ Sungka/app/src/main/java/uk/ac/kcl/teamraccoon/sungka/highscores/AddScoreFragment.java
@@ -29,13 +29,10 @@ public class AddScoreFragment extends DialogFragment {
 
     private int[] mScores;
     private View rootView;
-    boolean aiChosen;
 
     static final public String BUNDLE_TAG = "uk.ac.kcl.teamraccoon.sungka.AddScoreFragment.SCORES";
 
-    public AddScoreFragment(boolean aiChosen) {
-        this.aiChosen = aiChosen;
-    }
+    public AddScoreFragment() {}
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -49,12 +46,6 @@ public class AddScoreFragment extends DialogFragment {
         String statistics =  getString(R.string.add_score_players, mScores[0], mScores[1]);
 
         tvStatistics.setText(statistics);
-
-        if(aiChosen) {
-            EditText etPlayerTwo = (EditText) rootView.findViewById(R.id.player_two_name);
-            etPlayerTwo.setText(R.string.ai_name);
-            etPlayerTwo.setEnabled(false);
-        }
 
         builder.setView(rootView)
                 .setPositiveButton(R.string.save_score, new DialogInterface.OnClickListener() {
